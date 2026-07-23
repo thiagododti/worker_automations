a692cdbade63e12cd5418a333326effff411b7e5
Thiago Dantas <t.dod@live.com>
2026-07-23 08:53:55 -0300

    feat(consultas): adiciona retry com backoff exponencial
    Implementa lógica de retry para os módulos de consulta CNDT e FGTS da Inforsimples. Quando a API retorna código 605, o sistema realiza até 3 tentativas com espera exponencial (5s, 10s, 20s). Também refatora o tratamento de erros para usar getattr e simplifica a identificação do tipo de documento consultado.

--- app/automacoes/consulta_cndt/main.py
+++ app/automacoes/consulta_cndt/main.py
@@ -1,14 +1,23 @@
 class ConsultaCNDTInfoSimples:
     from app.automacoes.consulta_cndt.schema import EnvelopeConsultaCNDT
     from app.shared.botlogger import Botlogger
+    import os
+    import time
+    import requests
 
     def __init__(self, payload: EnvelopeConsultaCNDT):
-        import os
+        # Configurações de retry inforsimples
+        self.retry_count = 0
+        self.max_retries = 3
+        self.code = None
+        self.retry_sleep = 5
+        ######################################
 
+        # Configurações de consulta CNDT
         self.payload = payload
         self.url = "https://api.infosimples.com/api/v2/consultas/tribunal/tst/cndt"
         self.params = {
-            "token": os.getenv("INFOSIMPLES_TOKEN"),
+            "token": self.os.getenv("INFOSIMPLES_TOKEN"),
             "timeout": 600,
             "ignore_site_receipt": 0,
         }
@@ -18,39 +27,62 @@ class ConsultaCNDTInfoSimples:
             self.params["cpf"] = self.payload.cpf
         else:
             raise ValueError("É necessário fornecer um CNPJ ou CPF para a consulta.")
+        ######################################
 
     def execucao(self):
-        import requests
 
         try:
+            # Iiniciando o botlogger para registrar a execução da consulta
             botlogger = self.Botlogger(
-                empresa=self.payload.empresa_cnpj, automacao_id=27
+                empresa=self.payload.empresa_cnpj,
+                automacao_id=27,
             )
             botlogger.inicio_execucao()
-            botlogger.inicio_etapa(identificacao=self.payload.cnpj or self.payload.cpf)
+            botlogger.inicio_etapa(
+                identificacao=self.payload.cnpj or self.payload.cpf or "desconhecido",
+            )
 
-            response = requests.get(self.url, params=self.params, timeout=30)
-            dados = response.json()
+            # Loop de tentativas de consulta com retry
+            while self.retry_count < self.max_retries:
+                response = self.requests.get(self.url, params=self.params, timeout=30)
+                dados = response.json()
+                self.code = dados.get("code")
 
-            if self.payload.cnpj:
-                print(f"Consulta realizada para CNPJ: {self.payload.cnpj}")
-            elif self.payload.cpf:
-                print(f"Consulta realizada para CPF: {self.payload.cpf}")
-            if dados.get("code") == 200:
-                botlogger.fim_etapa()
-            else:
-                botlogger.alerta_etapa(
-                    f"Consulta retornou código {dados.get('code')} Consulta não foi realizada com sucesso."
-                )
-            botlogger.fim_execucao()
+                if self.code == 200:
+                    # Consulta realizada com sucesso, sai do loop
+                    botlogger.fim_etapa()
+                    botlogger.fim_execucao()
+                    break
+                elif self.code == 605:
+                    # Inforsimples orienta a dar retry quando o código 605 é retornado.
+                    # https://api.infosimples.com/consultas/docs#boas-praticas
+                    self.retry_count += 1
+                    self.time.sleep(self.retry_sleep)
+                    self.retry_sleep *= 2  # Aumenta o tempo de espera exponencialmente
+                else:
+                    # Qualquer outro código de erro, sai do loop e não tenta novamente
+                    botlogger.alerta_etapa(
+                        f"Consulta retornou código {self.code} Consulta não foi realizada com sucesso."
+                    )
+                    botlogger.alerta_execucao(
+                        f"Consulta retornou código {self.code} Consulta não foi realizada com sucesso."
+                    )
+                    break
+
             return dados
 
-        except requests.RequestException as e:
-            if self.payload.cnpj:
-                print(f"Erro ao consultar cndt {self.payload.cnpj}: {e}")
-                botlogger.erro_etapa(f"Erro ao consultar cndt {self.payload.cnpj}: {e}")
-            elif self.payload.cpf:
-                print(f"Erro ao consultar cndt {self.payload.cpf}: {e}")
-                botlogger.erro_etapa(f"Erro ao consultar cndt {self.payload.cpf}: {e}")
+        except self.requests.RequestException as e:
+            tipo = (
+                "cnpj"
+                if self.payload.cnpj
+                else "cpf"
+                if self.payload.cpf
+                else "desconhecido"
+            )
+
+            botlogger.erro_etapa(
+                f"Erro ao consultar cnd tst {getattr(self.payload, tipo)}: {e}"
+            )
+
             botlogger.erro_execucao(f"Execução com erro: {e}")
             return {"error": "Erro ao conectar com a API.", "data": str(e)}
--- app/automacoes/consulta_fgts/main.py
+++ app/automacoes/consulta_fgts/main.py
@@ -1,14 +1,23 @@
 class ConsultaFgtsInfoSimples:
     from app.automacoes.consulta_fgts.schema import EnvelopeConsultaFgts
     from app.shared.botlogger import Botlogger
+    import requests
+    import os
+    import time
 
     def __init__(self, payload: EnvelopeConsultaFgts):
-        import os
+        # Configurações de retry inforsimples
+        self.retry_count = 0
+        self.max_retries = 3
+        self.code = None
+        self.retry_sleep = 5
+        ######################################
 
+        # Configurações de consulta FGTS
         self.payload = payload
         self.url = "https://api.infosimples.com/api/v2/consultas/caixa/regularidade"
         self.params = {
-            "token": os.getenv("INFOSIMPLES_TOKEN"),
+            "token": self.os.getenv("INFOSIMPLES_TOKEN"),
             "timeout": 600,
             "ignore_site_receipt": 0,
             "preferencia_emissao": "nova",
@@ -19,45 +28,62 @@ class ConsultaFgtsInfoSimples:
             self.params["cpf"] = self.payload.cpf
         else:
             raise ValueError("É necessário fornecer um CNPJ ou CPF para a consulta.")
+        ######################################
 
     def execucao(self):
-        import requests
 
         try:
+            # Iiniciando o botlogger para registrar a execução da consulta
             botlogger = self.Botlogger(
                 empresa=self.payload.empresa_cnpj,
                 automacao_id=19,
             )
             botlogger.inicio_execucao()
             botlogger.inicio_etapa(
-                identificacao=self.payload.cnpj or self.payload.cpf or "consultando_api"
+                identificacao=self.payload.cnpj or self.payload.cpf or "desconhecido",
             )
-            response = requests.get(self.url, params=self.params, timeout=30)
-            dados = response.json()
-            if self.payload.cnpj:
-                print(f"Consulta realizada para CNPJ: {self.payload.cnpj}")
-            elif self.payload.cpf:
-                print(f"Consulta realizada para CPF: {self.payload.cpf}")
-            if dados.get("code") == 200:
-                botlogger.fim_etapa()
-            else:
-                botlogger.alerta_etapa(
-                    f"Consulta retornou código {dados.get('code')} Consulta não foi realizada com sucesso."
-                )
-            botlogger.fim_etapa()
+
+            # Loop de tentativas de consulta com retry
+            while self.retry_count < self.max_retries:
+                response = self.requests.get(self.url, params=self.params, timeout=30)
+                dados = response.json()
+                self.code = dados.get("code")
+
+                if self.code == 200:
+                    # Consulta realizada com sucesso, sai do loop
+                    botlogger.fim_etapa()
+                    break
+                elif self.code == 605:
+                    # Inforsimples orienta a dar retry quando o código 605 é retornado.
+                    # https://api.infosimples.com/consultas/docs#boas-praticas
+                    self.retry_count += 1
+                    self.time.sleep(self.retry_sleep)
+                    self.retry_sleep *= 2  # Aumenta o tempo de espera exponencialmente
+                else:
+                    # Qualquer outro código de erro, sai do loop e não tenta novamente
+                    botlogger.alerta_etapa(
+                        f"Consulta retornou código {self.code} Consulta não foi realizada com sucesso."
+                    )
+                    botlogger.alerta_execucao(
+                        f"Consulta retornou código {self.code} Consulta não foi realizada com sucesso."
+                    )
+                    break
+
             botlogger.fim_execucao()
             return dados
 
-        except requests.RequestException as e:
-            if self.payload.cnpj:
-                print(f"Erro ao consultar cnd fgts {self.payload.cnpj}: {e}")
-                botlogger.erro_etapa(
-                    f"Erro ao consultar cnd fgts {self.payload.cnpj}: {e}"
-                )
-            elif self.payload.cpf:
-                print(f"Erro ao consultar cnd fgts {self.payload.cpf}: {e}")
-                botlogger.erro_etapa(
-                    f"Erro ao consultar cnd fgts {self.payload.cpf}: {e}"
-                )
+        except self.requests.RequestException as e:
+            tipo = (
+                "cnpj"
+                if self.payload.cnpj
+                else "cpf"
+                if self.payload.cpf
+                else "desconhecido"
+            )
+
+            botlogger.erro_etapa(
+                f"Erro ao consultar cnd fgts {getattr(self.payload, tipo)}: {e}"
+            )
+
             botlogger.erro_execucao(f"Execução com erro: {e}")
             return {"error": "Erro ao conectar com a API.", "data": str(e)}
